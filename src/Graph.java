0a170458baec3271fab538593f25cba1b7979f26
Julien Schweter <13julien37@gmail.com>
2022-06-14 13:56:02 +0200

    Add graph class functionality

--- src/Graph.java
+++ src/Graph.java
@@ -1,7 +1,67 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class for a graph data structure using an adjacency list.
+ * Edges of vertices will be stored undirected.
+ */
 public class Graph {
-    int numVertices;
-    List<List<Integer>> adj_list = new ArrayList<>();
+    private final int numVertices;
+    private final List<List<Integer>> adjList;
+
+    /**
+     *
+     * @param numberOfVertices Number of vertices in the graph.
+     */
+    public Graph(int numberOfVertices){
+        adjList = new ArrayList<>();
+        this.numVertices = numberOfVertices;
+
+        for(int i = 0; i < numberOfVertices; i++) {
+            ArrayList<Integer> neighbours = new ArrayList<>();
+            adjList.add(neighbours);
+        }
+    }
+
+    /**
+     * Will insert an undirected edge into the adjacency list of this class.
+     * @param u First vertex of the edge
+     * @param v Second vertex of the edge
+     */
+    public void addEdge(int u, int v){
+        if(adjList.get(u) != null || adjList.get(v) != null){
+            // Only add vertex as neighbour if it isn't already a neighbour.
+            if(!adjList.get(u).contains(v)) {
+                adjList.get(u).add(v);
+            }
+
+            // Graph is not directional, so add the vertex to the other vertex as a neighbour as well.
+            if(!adjList.get(v).contains(u)) {
+                adjList.get(v).add(u);
+            }
+
+        } else {
+            throw new ArrayIndexOutOfBoundsException("Vertex does not exist in adjacency list.");
+        }
+    }
+
+    /**
+     * Returns a list of neighbours from a specific vertex.
+     * Will include the vertex itself as well, as the graph is not directional.
+     * @param vertex Vertex to get the neighbours from.
+     * @return The neighbours of the vertex as a list.
+     */
+    public List<Integer> getNeighbours(int vertex) {
+        return adjList.get(vertex);
+    }
+
+    /**
+     * Returns the number of vertices in the graph.
+     * @return Number of vertices in the graph.
+     */
+    public int getNumVertices(){
+        return numVertices;
+    }
+
+
 }
