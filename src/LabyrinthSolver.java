11709df0f88fad33e673fa8cf80c50b1d7f97f63
franco <franco.zipfel@gmx.de>
2022-06-15 10:31:22 +0200

    add base for class LabyrinthSolver

---
+++ src/LabyrinthSolver.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Stack;
+
+public class LabyrinthSolver {
+
+    private Graph graph;
+    private int startVertex;
+    private int endVertex;
+    private List<Stack<Integer>> paths;
+
+    public LabyrinthSolver(Graph graph) {
+
+    }
+
+    public void printAllPaths(int startVertex, int endVertex) {
+
+    }
+
+    private void searchForVertex(int vertex, Stack<Integer> visited) {
+
+    }
+}
