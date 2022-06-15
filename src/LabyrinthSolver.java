6d874d542b1885eb275f6703abc95ce48021e30d
franco <franco.zipfel@gmx.de>
2022-06-15 13:49:09 +0200

    add body of methods

--- src/LabyrinthSolver.java
+++ src/LabyrinthSolver.java
@@ -9,14 +9,33 @@ public class LabyrinthSolver {
     private List<Stack<Integer>> paths;
 
     public LabyrinthSolver(Graph graph) {
-
+        this.graph = graph;
     }
 
     public void printAllPaths(int startVertex, int endVertex) {
-
+        this.startVertex = startVertex;
+        this.endVertex = endVertex;
+        Stack<Integer> path = new Stack<>();
+        searchForVertex(startVertex, path);
     }
 
     private void searchForVertex(int vertex, Stack<Integer> visited) {
 
+        // funktioniert noch nicht... später noch weiter bearbeiten
+
+        // look if current vertex is searched vertex
+        if (vertex == endVertex) {
+            visited.push(vertex);
+            paths.add(visited);
+            return;
+        }
+
+        // look if circlefree and if not move on
+        if (!visited.contains(vertex)) {
+            visited.push(vertex);
+            for (Integer neighbour : graph.getNeighbours(vertex)) {
+                searchForVertex(neighbour, visited);
+            }
+        }
     }
 }
