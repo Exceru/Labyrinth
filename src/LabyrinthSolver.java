db3f0ade939a836762b79fa65406ac10e6802705
franco <franco.zipfel@gmx.de>
2022-06-17 15:31:38 +0200

    add new working body of methods

--- src/LabyrinthSolver.java
+++ src/LabyrinthSolver.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 public class LabyrinthSolver {
 
-    private Graph graph;
+    private final Graph graph;
     private int startVertex;
     private int endVertex;
     private List<Stack<Integer>> paths;
 
     public LabyrinthSolver(Graph graph) {
+        paths = new ArrayList<>();
         this.graph = graph;
     }
 
@@ -17,25 +19,27 @@ public class LabyrinthSolver {
         this.endVertex = endVertex;
         Stack<Integer> path = new Stack<>();
         searchForVertex(startVertex, path);
+
+        for (Stack<Integer> visited : paths) {
+            if (!visited.isEmpty()) {
+                System.out.println(visited);
+            }
+        }
     }
 
     private void searchForVertex(int vertex, Stack<Integer> visited) {
 
-        // funktioniert noch nicht... später noch weiter bearbeiten
-
-        // look if current vertex is searched vertex
-        if (vertex == endVertex) {
-            visited.push(vertex);
-            paths.add(visited);
-            return;
-        }
-
-        // look if circlefree and if not move on
         if (!visited.contains(vertex)) {
             visited.push(vertex);
-            for (Integer neighbour : graph.getNeighbours(vertex)) {
-                searchForVertex(neighbour, visited);
+            if (vertex == endVertex) {
+                System.out.println(visited);
+                paths.add(visited);
+            } else {
+                for (Integer neighbour : graph.getNeighbours(vertex)) {
+                    searchForVertex(neighbour, visited);
+                }
             }
+            visited.pop();
         }
     }
 }
